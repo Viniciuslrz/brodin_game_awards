40d41673affa0cbc59a7d0874368b7c5c63e7f24
viniciuslrz <vinicius_lorenz@hotmail.com>
2024-10-26 21:01:52 -0300

    Reworking game lists

--- src/data/FogoDePalhaDoAno.ts
+++ src/data/FogoDePalhaDoAno.ts
@@ -2,21 +2,18 @@ import type { Game } from "@/types/Game";
 
 export const FogoDePalhaDoAno:Game[] =[
     {id: 1, name:"Chained Together", check: false},
-    {id: 2, name:"Dead by Daylight", check: false},
     {id: 3, name:"Deadlock", check: false},
-    {id: 4, name:"Diablo IV", check: false},
-    {id: 5, name:"Farcry 6", check: false},
     {id: 6, name:"Goose Goose Duck", check: false},
     {id: 7, name:"Lethal Company", check: false},
     {id: 8, name:"Monster Hunter Rise", check: false},
-    {id: 9, name:"Phasmophobia", check: false},
     {id: 10, name:"Pokemon GO", check: false},
     {id: 11, name:"Pokemon TCG", check: false},
-    {id: 12, name:"Pokemon Unite", check: false},
-    {id: 13, name:"Project Winter", check: false},
-    {id: 14, name:"Quidditch Champions", check: false},
-    {id: 15, name:"Rainbow Six Siege", check: false},
-    {id: 16, name:"Throne and Liberty", check: false},
-    {id: 17, name:"World of Warcraft", check: false},
-    {id: 18, name:"Wuthering Waves", check: false}
 ];
+
+/*
+{id: 2, name:"Dead by Daylight", check: false},
+{id: 9, name:"Phasmophobia", check: false},
+{id: 12, name:"Pokemon Unite", check: false},
+{id: 13, name:"Project Winter", check: false},
+{id: 15, name:"Rainbow Six Siege", check: false},
+*/
--- src/data/TochaOlimpica.ts
+++ src/data/TochaOlimpica.ts
@@ -4,14 +4,11 @@ export const TochaOlimpica:Game[] =[
     {id: 1, name:"Dead by Daylight", check: false},
     {id: 2, name:"Final Fantasy XIV", check: false},
     {id: 3, name:"Fortnite", check: false},
-    {id: 4, name:"KTANE", check: false},
     {id: 5, name:"Honkai Star Rail", check: false},
     {id: 6, name:"Overwatch", check: false},
     {id: 7, name:"Phasmophobia", check: false},
     {id: 8, name:"Pokemon GO", check: false},
     {id: 9, name:"Pokemon TCG", check: false},
     {id: 10, name:"Pokemon Unite", check: false},
-    {id: 11, name:"Rainbow Six Siege", check: false},
-    {id: 12, name:"World of Warcraft", check: false},
-    {id: 13, name:"Yu-Gi-Oh Duel Links", check: false}
+    {id: 11, name:"Rainbow Six Siege", check: false}
 ];
